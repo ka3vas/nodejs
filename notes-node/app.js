77ed2dc4ce1797b8a03694c6d72e5d44afd7744b
ka3vas <tyrnis@gmail.com>
2018-12-28 11:54:35 +0100

    first commit

---
+++ notes-node/app.js
@@ -0,0 +1,15 @@
+console.log("Starting app");
+
+const fs = require("fs");
+const os = require("os");
+
+var user = os.userInfo();
+
+fs.appendFile("greetings.txt", `Hello ${user.username}!`, function(err) {
+  if (err) {
+    console.log("Unable to write to file!");
+  }
+});
+
+// OR
+// fs.appendFileSync("greetings.txt", "Hello world!");
