023ce381d4ff49a34777dcac0dba1e5b20146bfa
ka3vas <tyrnis@gmail.com>
2018-12-28 13:54:17 +0100

    cleaned up files, added nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,32 +1,6 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const os = require("os");
 const _ = require("lodash");
+
 const notes = require("./notes.js");
-
-var filteredArr = _.uniq(["Sebastian", 1, "Sebastian", 1, 2, 3, 4]);
-console.log(filteredArr);
-
-// console.log(_.isString(true));
-// console.log(_.isString("true"));
-
-// var res = notes.addNote();
-// console.log(res);
-
-// console.log("Result:", notes.add(9, -2));
-
-// var user = os.userInfo();
-
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello ${user.username}! You are ${notes.age}.`,
-//   function(err) {
-//     if (err) {
-//       console.log("Unable to write to file!");
-//     }
-//   }
-// );
-
-// OR
-// fs.appendFileSync("greetings.txt", "Hello world!");
