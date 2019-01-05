480d3b81586d01a7bed4a30e2a58402ae0ec7d59
Bastian <tyrnis@gmail.com>
2019-01-05 13:27:31 +0100

    weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,6 +7,8 @@ request(
     json: true
   },
   (error, response, body) => {
-    console.log(JSON.stringify(body, null, 2));
+    console.log(`Address: ${body.results[0].providedLocation.location}`);
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitude: , lng: ${body.results[0].locations[0].latLng.lng}`);
   }
 );
