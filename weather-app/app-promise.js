29cc2a50b099d137bae810d2e068775a0f8f4bfa
ka3vas <tyrnis@gmail.com>
2019-01-06 17:00:24 +0100

    added axios

---
+++ weather-app/app-promise.js
@@ -0,0 +1,51 @@
+const yargs = require("yargs");
+const axios = require("axios");
+
+const apiKeys = require("./api/api-keys");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather from.",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${
+  apiKeys.geocode
+}&location=${encodedAddress}`;
+
+axios
+  .get(geocodeUrl)
+  .then(response => {
+    if (response.data.info.statuscode !== 0) {
+      throw new Error("Unable to find that address.");
+    }
+
+    const { lat, lng } = response.data.results[0].locations[0].latLng;
+    const { location } = response.data.results[0].providedLocation;
+    const weatherUrl = `
+      https://api.darksky.net/forecast/${apiKeys.forecast}/${lat},${lng}
+    `;
+
+    console.log(location);
+    return axios.get(weatherUrl);
+  })
+  .then(response => {
+    const { temperature, apparentTemperature } = response.data.currently;
+    console.log(
+      `Its currently ${temperature}, but it feels like ${apparentTemperature}.`
+    );
+  })
+  .catch(err => {
+    if (err.code === "ENOTFOUND") {
+      console.log("Unable to connect to API servers.");
+    } else {
+      console.log(err.message);
+    }
+  });
