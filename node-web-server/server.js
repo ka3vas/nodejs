b73c7367f764a54b2dc3b9c44a490694f456b5b4
ka3vas <tyrnis@gmail.com>
2019-01-16 20:53:23 +0100

    whatever

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 const app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+// simple middleware for tracking how server is working
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs", {
+//     pageTitle: "Maintenance"
+//   });
+// });
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
